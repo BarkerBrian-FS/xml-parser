e096bca4a85b97cbaec091ae44e1e3b267a78abc
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-09-21 13:26:24 -0500

    added new get route for api to get documents

--- server/src/server.js
+++ server/src/server.js
@@ -4,29 +4,51 @@ const express = require("express");
 const cors = require("cors");
 const { analyzeXML } = require("./services/xmlService");
 const connectDB = require("./config/db");
+const Document = require("./models/Documents.js");
 
 const app = express();
 app.use(cors());
 app.use(express.text({ type: "application/xml" }));
 
 const PORT = 5000;
-connectDB();
+connectDB(process.env.MONGO_URI);
+
 app.get("/api/health", (req, res) => {
   res.json({
     message: "API is running",
   });
 });
 
-app.post("/api/xml/analyze", (req, res) => {
-  const result = analyzeXML(req.body);
+app.get("/api/xml/documents", async (req, res) => {
+  try {
+    const documents = await document.find();
 
+    res.json(documents);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Failed to retrieve documents",
+    });
+  }
+});
+
+app.post("/api/xml/analyze", async (req, res) => {
   console.log("POST route reached");
 
-  console.log(typeof req.body);
-  console.log(req.body);
+  try {
+    const result = analyzeXML(req.body);
 
-  console.log(result);
-  res.json(result);
+    const document = await Document.create(result);
+
+    console.log(document);
+
+    res.json(document);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Failed to save document",
+    });
+  }
 });
 
 app.listen(PORT, () => {
