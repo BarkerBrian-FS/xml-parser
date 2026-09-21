8a08e01a1c4232f29495d7fd55a2ba1348b0ff49
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-09-21 13:00:43 -0500

    added mongodb community and db.js file

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,16 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const { analyzeXML } = require("./services/xmlService");
+const connectDB = require("./config/db");
 
 const app = express();
 app.use(cors());
 app.use(express.text({ type: "application/xml" }));
 
 const PORT = 5000;
-
+connectDB();
 app.get("/api/health", (req, res) => {
   res.json({
     message: "API is running",
